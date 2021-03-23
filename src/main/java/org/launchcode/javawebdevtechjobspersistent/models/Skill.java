1ffc66cc3d151f1f35480366072f8e29efd0b224
Becca Johnson <beccanjohnson@gmail.com>
2021-03-22 19:11:54 -0500

    -m "completed AbstractEntity     completed Employer     completed Skill"

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    public String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Skill () {}
 
 }
