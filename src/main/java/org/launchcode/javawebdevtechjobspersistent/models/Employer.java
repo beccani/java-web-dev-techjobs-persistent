bfb366d4e8838e808633529717631af3974788e9
Becca Johnson <beccanjohnson@gmail.com>
2021-03-31 21:50:00 -0500

    -m "finished through part4"

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,13 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.ArrayList;
+
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +15,10 @@ public class Employer extends AbstractEntity {
     @NotBlank(message="Location is required!")
     @Size(min=3, max=50, message="Location must be between 3 and 50 characters.")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
     public String getLocation() {
         return location;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,11 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     public String description;
+
+    @ManyToMany(mappedBy = "skills")
+    public List<Job> jobs = new ArrayList<>();
+
+    public Skill(String description, List<Job> jobs) {
+        this.description = description;
+        this.jobs = jobs;
+    }
 
     public String getDescription() {
         return description;
@@ -16,5 +27,13 @@ public class Skill extends AbstractEntity {
     }
 
     public Skill () {}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
 }
